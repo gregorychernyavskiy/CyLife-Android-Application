9b470aff3a431cc286b606bd0f24c82b2142bc8e
Seth Clover <sclover@iastate.edu>
2024-11-06 17:59:09 -0600

    added socket for student joining clubs

---
+++ Backend/CyLife/src/main/java/CyLife/Websockets/joinClub/MessageRepository.java
@@ -0,0 +1,7 @@
+package CyLife.Websockets.joinClub;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Long>{
+
+}
---
+++ Backend/CyLife/src/main/java/CyLife/Websockets/joinClub/StudentJoinSocket.java
@@ -0,0 +1,87 @@
+package CyLife.Websockets.joinClub;
+
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.PathParam;
+import jakarta.websocket.server.ServerEndpoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller      // this is needed for this to be an endpoint to springboot
+@ServerEndpoint(value = "/joinClub/{clubId}/{studentName}")  // this is Websocket url
+public class StudentJoinSocket {
+
+    // cannot autowire static directly (instead we do it by the below
+    // method
+    private static MessageRepository msgRepo;
+
+    /*
+     * Grabs the MessageRepository singleton from the Spring Application
+     * Context.  This works because of the @Controller annotation on this
+     * class and because the variable is declared as static.
+     * There are other ways to set this. However, this approach is
+     * easiest.
+     */
+    @Autowired
+    public void setMessageRepository(MessageRepository repo) {
+        msgRepo = repo;  // we are setting the static variable
+    }
+
+    // Store all socket session and their corresponding username.
+    private static Map<Session, String> sessionUsernameMap = new Hashtable<>();
+    private static Map<String, Session> usernameSessionMap = new Hashtable<>();
+
+    private final Logger logger = LoggerFactory.getLogger(StudentJoinSocket.class);
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("clubId") String clubId, @PathParam("studentName") String studentName) throws IOException {
+        logger.info("Entered into Open");
+
+        // store connecting user information
+        sessionUsernameMap.put(session, clubId);
+        usernameSessionMap.put(clubId, session);
+
+        broadcast(studentName + " has joined the club!");
+    }
+
+    @OnClose
+    public void onClose(Session session) throws IOException {
+        logger.info("Entered into Close");
+
+        // remove the user connection information
+        String username = sessionUsernameMap.get(session);
+        sessionUsernameMap.remove(session);
+        usernameSessionMap.remove(username);
+    }
+
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        // Do error handling here
+        logger.info("Entered into Error");
+        throwable.printStackTrace();
+    }
+
+    private void broadcast(String message) {
+        sessionUsernameMap.forEach((session, username) -> {
+            try {
+                session.getBasicRemote().sendText(message);
+            }
+            catch (IOException e) {
+                logger.info("Exception: " + e.getMessage().toString());
+                e.printStackTrace();
+            }
+        });
+    }
+}
